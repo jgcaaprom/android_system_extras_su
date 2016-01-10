4130bac49d3d8a72d37c66d051f3943b6668a7af
Jorge Pereira <jgcaap@gmail.com>
2016-01-10 20:12:15 +0000

    su to work on userbuilds
    Change-Id: I01164cba4a5ccf464aa037c2cb920dc5f20f4c84

--- Android.mk
+++ Android.mk
@@ -5,7 +5,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := su
-LOCAL_MODULE_TAGS := optional
 LOCAL_WHOLE_STATIC_LIBRARIES := libcutils libutils libbinder liblog
 LOCAL_SRC_FILES := su.c daemon.c utils.c pts.c
 LOCAL_SRC_FILES += binder/appops-wrapper.cpp binder/pm-wrapper.c
@@ -13,12 +12,16 @@ LOCAL_CFLAGS += -Werror
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_EXECUTABLE)
 
+
+$(TARGET_OUT)/xbin/su-post: su
+	chmod ug+s $(TARGET_OUT_OPTIONAL_EXECUTABLES)/su
+
 SYMLINKS := $(addprefix $(TARGET_OUT)/bin/,su)
 $(SYMLINKS):
 	@echo "Symlink: $@ -> /system/xbin/su"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf ../xbin/su $@
+	ln -sf ../xbin/su $@
 
 # make sure init.superuser.rc is imported from
 # init.rc or similar
@@ -31,4 +34,3 @@ $(SUPERUSER_RC): $(LOCAL_PATH)/init.superuser.rc | $(ACP)
 # local module name
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
     $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS) $(SUPERUSER_RC)
-
